9fa9eecf82d8b01ec05131aab42f4a9db2dafde5
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-04 04:28:57 +0100

    hot fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python3
-""" Flask application """
+"""
+Parametrize templates
+"""
 
-
+import babel
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
+
 
 class Config:
-    """ l18n Config class """
-    LANGUAGES = ["en", "fr"]
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
-app.config.from_object("1-app.Config")
-babel = Babel(app)
+app.config.from_object(Config)
 
 
-@app.route("/")
-def hello_world():
-    """ Handle default route """
-    return render_template("3-index.html")
-
-
-
+@babel.localeselector
 def get_locale():
-    """ Gets the best matching language for user """
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    """
+     determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel = Babel(app, locale_selector=get_locale)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
